a7bcc89d0695b461794f4bb95ced37ad288d2505
WANG Jiaqi <mine4ghost@gmail.com>
2015-05-11 22:36:47 +0800

    加入ua,防止屏蔽

--- getip.py
+++ getip.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 from pyquery import PyQuery as pq
-d = pq('https://www.whatismyip.com/my-ip-information/')
+import urllib2
+opener = urllib2.build_opener()
+opener.addheaders = [('User-agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36')]
+page = opener.open('https://www.whatismyip.com/my-ip-information/')
+#d = pq('https://www.whatismyip.com/my-ip-information/')
+d = pq(page.read())
 ip = d('.ip').text()
 print "Your Ip Address is:",
 print "".join(ip.split(':')[1].split())
